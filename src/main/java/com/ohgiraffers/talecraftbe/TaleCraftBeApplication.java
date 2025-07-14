5f390d0c9f07b07ca1eab4c48f23c1863f855ead
kjs9504 <emflazlwm62@gmail.com>
2025-07-14 12:56:15 +0900

    시작

---
+++ src/main/java/com/ohgiraffers/talecraftbe/TaleCraftBeApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.talecraftbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaleCraftBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaleCraftBeApplication.class, args);
+	}
+
+}
