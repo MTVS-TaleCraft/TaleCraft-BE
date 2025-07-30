073574f4e148690413e244d5e38a3aead8fb10d2
smg0218 <smg000218@gmail.com>
2025-07-30 15:35:05 +0900

    chore : AI 통신에 swagger 설명 추가

--- src/main/java/com/talecraft/talecraftbe/ai/controller/AIController.java
+++ src/main/java/com/talecraft/talecraftbe/ai/controller/AIController.java
@@ -5,12 +5,19 @@ import com.talecraft.talecraftbe.ai.dto.response.AddAIResponseDTO;
 import com.talecraft.talecraftbe.ai.dto.response.FindAIResponseDTO;
 import com.talecraft.talecraftbe.ai.exception.AIRequestFailException;
 import com.talecraft.talecraftbe.ai.service.AIService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Slf4j
 @RestController
@@ -26,18 +33,21 @@ public class AIController {
     public ResponseEntity<?> getAi(@PathVariable Long episodeId, @RequestParam Long chatListId) {
         // AI와 대화한 기록 출력
         log.info("GET : /api/ai/{}" , episodeId);
-        if(episodeId == null && chatListId == null) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("status", false);
-            response.put("message", "에피소드ID 또는 챗리스트ID 중 하나는 필수로 주어야 합니다!");
-            return ResponseEntity.badRequest().body(response);
-        }
         FindAIResponseDTO chatList = aiService.getChatList(episodeId, chatListId);
 
         return ResponseEntity.ok().body(chatList);
     }
 
-    @PostMapping
+    @Operation(summary = "AI 사용 API", description = "AI를 사용하기 위한 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "AI 응답 성공",
+                    content = @Content(schema = @Schema(implementation = AddAIResponseDTO.class))),
+            @ApiResponse(responseCode = "401", description = "AI 응답 실패[에피소드ID나 챗ID를 찾지 못함]",
+            content = @Content(schema = @Schema(implementation = AddAIResponseDTO.class))),
+            @ApiResponse(responseCode = "500", description = "AI 통신 실패",
+            content = @Content(schema = @Schema(implementation = AddAIResponseDTO.class))),
+    })
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public ResponseEntity<?> postAi(@ModelAttribute AddAIRequestDTO requestDTO) {
         log.info("POST : /api/ai");
         log.info("requestDTO: {}", requestDTO);
@@ -54,7 +64,17 @@ public class AIController {
         return ResponseEntity.ok().body(responseAIDTO);
     }
 
-    @ExceptionHandler
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<?> handleNoSuchElementException(NoSuchElementException e) {
+        log.error(e.getMessage());
+
+        AddAIResponseDTO responseAIDTO = new AddAIResponseDTO();
+        responseAIDTO.setStatus(false);
+        responseAIDTO.setResponse(e.getMessage());
+
+        return ResponseEntity.badRequest().body(responseAIDTO);
+    }
+    @ExceptionHandler(AIRequestFailException.class)
     public ResponseEntity<?> handleAIRequestFailException(AIRequestFailException e) {
         log.error(e.getMessage());
 
--- src/main/java/com/talecraft/talecraftbe/ai/dto/request/AddAIRequestDTO.java
+++ src/main/java/com/talecraft/talecraftbe/ai/dto/request/AddAIRequestDTO.java
@@ -2,6 +2,7 @@ package com.talecraft.talecraftbe.ai.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.talecraft.talecraftbe.ai.dto.data.AIOptions;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,13 +16,22 @@ import java.util.List;
 @EqualsAndHashCode
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class AddAIRequestDTO {
+    @Schema(description = "회차 아이디", example = "1")
     private Long episodeId;
+    @Schema(description = "챗목록 아이디", example = "1")
     private Long chatListId;
+    @Schema(description = "QA형식 사용여부", example = "false")
     private boolean useChatList;
+    @Schema(description = "질문", example = "여기에 질문 입력")
     private String question;
+    @Schema(description = "이전 질문 목록")
     private List<String> beforeQuestionList;
+    @Schema(description = "이전 응답 목록")
     private List<String> beforeResponseList;
+    @Schema(description = "AI 옵션", example = "NORMAL", allowableValues = {"NORMAL", "SPELL_CHECK", "STORY_EXTENSION", "MAKE_NAME"})
     private AIOptions option;
+    @Schema(description = "이야기 길이 늘리기 최소 값", example = "6000", maximum = "15000")
     private Integer extensionLength;
+    @Schema(description = "AI가 참고할 이미지[현재 사용 불가능]", type = "string", format = "binary")
     private MultipartFile image;
 }
