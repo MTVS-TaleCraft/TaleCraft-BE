8cf7731a7a5f819bde9812ea8ea2f64f9985795b
kjs9504 <emflazlwm62@gmail.com>
2025-08-05 11:59:32 +0900

    포트설정

--- src/main/java/com/talecraft/talecraftbe/auth/config/SecurityConfig.java
+++ src/main/java/com/talecraft/talecraftbe/auth/config/SecurityConfig.java
@@ -43,10 +43,11 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
                 "https://tale-craft-three.vercel.app",
-                "https://tale-craft-cnbbfdayr-emflazlwm62-9053s-projects.vercel.app",
                 "http://localhost:3000",  // 로컬 개발용
                 "http://localhost:8080",  // 필요하다면 추가
-                "http://localhost:8081"
+                "http://localhost:8081",  // 로컬 백엔드
+                "http://52.78.166.172:8080",  // EC2 서버
+                "https://52.78.166.172:8080"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
